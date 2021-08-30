b4dc6072ac5a79c5e2bca4646646d603d254493d
ichhjkim <52682733+ichhjkim@users.noreply.github.com>
2021-08-30 22:11:06 +0900

    Update 튜플.py
    - 정규표현식
    - colliecionts Counter

--- KAKAO/2019_튜플/튜플.py
+++ KAKAO/2019_튜플/튜플.py
@@ -23,5 +23,18 @@ def solution(s):
 
     return answer
 
+# 충격적인 다른분 풀이... 
+# 가장 많이 나온 원소 순대로 정렬하면.. 되는 거여뜸...
+
+import re
+from collections import Counter
+
+def solution2(s):
+
+    s = Counter(re.findall('\d+', s))
+    return list(map(int, [k for k, v in sorted(s.items(), key=lambda x: x[1], reverse=True)]))
+
+
+
 solution("{{2},{2,1},{2,1,3},{2,1,3,4}}")
 solution("{{4,2,3},{3},{2,3,4,1},{2,3}}")
